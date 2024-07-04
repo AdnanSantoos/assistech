902f4579001d389285a1b47ac879f2195260ee12
Adnan Pinho <adnanpinho@hotmail.com>
2024-07-04 14:14:32 -0300

    mobile menu

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule,MatIconModule,CommonModule],
+  providers:[BrowserAnimationsModule],
+  animations: [
+    trigger('toggleMenu', [
+      state('inactive', style({
+        opacity: 0,
+        transform: 'translateX(-100%)'
+      })),
+      state('active', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('inactive => active', [
+        animate('300ms ease-in')
+      ]),
+      transition('active => inactive', [
+        animate('300ms ease-out')
+      ])
+    ])
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  mobile = false;
+
+  toggleMenu() {
+    this.mobile = !this.mobile;
+  }
 
 }
