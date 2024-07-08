bb327b7a04b8a6e2b487aa695981fdaaa662bc1c
Rayan Silva <rayan.lima@shopdelivery.com.br>
2024-07-08 11:47:45 -0300

    acesso a informação com todos os textos, faltam os links das páginas

--- src/app/features/acesso-informacao-transparencia/acesso-informacao-transparencia.component.ts
+++ src/app/features/acesso-informacao-transparencia/acesso-informacao-transparencia.component.ts
@@ -7,128 +7,217 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-acesso-informacao-transparencia',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, FooterComponent,RouterLink],
+  imports: [CommonModule, NavbarComponent, FooterComponent, RouterLink],
   templateUrl: './acesso-informacao-transparencia.component.html',
   styleUrl: './acesso-informacao-transparencia.component.scss'
 })
 export class AcessoInformacaoTransparenciaComponent {
   button = [
     {
-      name: 'Ata das sessões',
-      link: '/ata-das-sessoes'
+      name: 'Agenda do Presidente',
+      link: '/agenda-presidente'
     },
     {
-      name: 'atos admissionais',
-      link: '/atos-admissionais'
+      name: 'Balanço geral',
+      link: '/balanco-geral'
     },
     {
-      name: 'audiência pública',
-      link: '/audiencias-publicas'
-    },
-    {
-      name: 'balancete',
+      name: 'Balancete Fiananceiro',
       link: '/balancete-financeiro'
     },
     {
-      name: 'Balanço Anual',
-      link: 'https://www.google.com.br/'
+      name: 'Carta de Serviço ao usuário',
+      link: '/carta-servico-usuario'
     },
     {
-      name: 'boletim de votação',
-      link: '/boletim-votacao'
+      name: 'Comissões',
+      link: '/comissoes'
     },
     {
-      name: 'contas anuais',
-      link: 'https://www.google.com.br/'
-    },
-    {
-      name: 'contratação direta',
-      link: '/contratacao-direta'
-    },
-    {
-      name: 'contratos',
-      link: '/contratos'
-    },
-    {
-      name: 'convênios',
+      name: 'Convênios',
       link: '/convenios'
     },
     {
-      name: 'dadods da frota',
-      link: 'https://www.google.com.br/'
+      name: 'Cotas para exercício de atividade parlamentar',
+      link: '/cotas-exercicio-atividade-parlamentar'
     },
     {
-      name: 'decretos e leis',
-      link: '/legislacao-municipal'
+      name: 'Dados Abertos',
+      link: '/dados-abertos'
     },
     {
-      name: 'diária',
-      link: '/diaria'
+      name: 'despesas',
+      link: '/despesas'
     },
     {
-      name: 'editais',
-      link: '/editais'
+      name: 'documentos administrativos',
+      link: '/documentos-administrativos'
     },
     {
-      name: 'folha de pagamento',
-      link: 'https://www.google.com.br/'
+      name: 'Documentos e informações sigilosas',
+      link: '/documentos-informacoes-sigilosas'
     },
     {
-      name: 'indicação e requerimento',
-      link: '/indicacoes-requerimentos'
+      name: 'estrutura organizacional',
+      link: '/estrutura-organizacional'
     },
     {
-      name: 'indicação e deliberação',
-      link: '/indicacao-deliberacao'
+      name: 'fale conosco',
+      link: '/fale-conosco'
     },
     {
-      name: 'instrumento de planejamento',
-      link: 'https://www.google.com.br/'
+      name: 'fiscal de contrato',
+      link: '/fiscal-contrato'
     },
     {
-      name: 'Licitações',
-      link: '/licitacoes'
+      name: 'julgamento das contas da prefeitura municipal',
+      link: '/julgamento-contas-prefeitura'
     },
     {
-      name: 'Lista de presença',
-      link: '/lista-presenca'
+      name: 'LGPD',
+      link: '/lgpd'
     },
     {
-      name: 'Moções',
-      link: '/mocoes'
+      name: 'Lei de diretrizes orçamentárias',
+      link: '/LDO'
     },
     {
-      name: 'Ofício',
-      link: '/oficio'
+      name: 'LEI ORÇAMENTÁRIA ANUAL',
+      link: '/LOA'
     },
     {
-      name: 'patrimônios públicos',
-      link: '/patrimonios-publicos'
+      name: 'Lei orgânica municipal',
+      link: '/lei-organica-municipal'
     },
     {
-      name: 'pedidos de providência',
-      link: '/pedidos-providencia'
+      name: 'legislação de pessoal do municipio',
+      link: '/legislacao-pessoal-municipio'
     },
     {
-      name: 'receitas e despesas',
-      link: 'https://www.google.com.br/'
+      name: 'Licitações / Contratos',
+      link: '/licitacoes-contratos'
     },
     {
-      name: 'RREO E RGF',
-      link: '/rreo-rgf'
+      name: 'Lista de Preposições e presença',
+      link: '/lista-preposicoes-presenca'
     },
     {
-      name: 'SIST. DE Registro de preço',
-      link: '/registro-preco'
+      name: 'Mapa do site',
+      link: '/mapa-site'
     },
     {
-      name: 'transferência de recursos',
-      link: '/transferencia-recursos'
+      name: 'Matérias legislativas',
+      link: '/materias-legislativas'
     },
     {
-      name: 'visão geral',
-      link: 'https://www.google.com.br/'
+      name: 'normas jurídicas',
+      link: 'normas-juridicas'
+    },
+    {
+      name: 'ordem cronológica dos pagamentos',
+      link: '/ordem-cronologica-pagamentos'
+    },
+    {
+      name: 'ouvidoria',
+      link: '/ouvidoria'
+    },
+    {
+      name: 'perguntas frequentes',
+      link: '/faq'
+    },
+    {
+      name: 'pesquisa de satisfação dos serviços',
+      link: '/pesquisa-satisfacao-servicos'
+    },
+    {
+      name: 'plano estratégico',
+      link: '/plano-estrategico'
+    },
+    {
+      name: 'plano plurianual',
+      link: '/ppa'
+    },
+    {
+      name: 'programa e ações',
+      link: '/programa-acoes'
+    },
+    {
+      name: 'parecer prévio de julgamento de contas',
+      link: '/parecer-previo-julgamento-contas'
+    },
+    {
+      name: 'Projetos e execuções de obras públicas',
+      link: '/projetos-execucoes-obras-publicas'
+    },
+    {
+      name: 'plano de contratações anual',
+      link: '/plano-contratacoes-anual'
+    },
+     {
+      name: 'relação dos licitantes contratados sancionados',
+      link: '/relacao-licitantes-contratados-sancionados'
+    },
+    {
+      name: 'radar nacional de transparência pública',
+      link: '/radar-nacional-transparencia-publica'
+    },
+    {
+      name: 'regulamentação da concessão de diárias',
+      link: '/regulamentacao-concessao-diarias'
+    },
+    {
+      name: 'regulamentação da ouvidoria',
+      link: '/regulamentacao-ouvidoria'
+    },
+    {
+      name: 'regulamentação da lai',
+      link: '/regulamentacao-lai'
+    },
+    {
+      name: 'Relação do patrimônio público (imóveis)',
+      link: '/relacao-patrimonio-publico-imoveis'
+    },
+    {
+      name: 'relação do patrimônio público móveis',
+      link: '/relacao-patrimonio-publico-moveis'
+    },
+    {
+      name: 'relatório de gestão e atividades',
+      link: '/relatorio-gestao-atividades'
+    },
+    {
+      name: 'relatório do controle interno',
+      link: '/relatorio-controle-interno'
+    },
+    {
+      name: 'relatório do e-sic',
+      link: '/relatorio-esic'
+    },
+    {
+      name: 'relatório de gestão fiscal',
+      link: '/relatorio-gestao-fiscal'
+    },
+    {
+      name: 'relatórios de atividades dos parlamentares',
+      link: '/relatorio-atividades-parlamentares'
+    },
+    {
+      name: 'repasses',
+      link: '/repasses'
+    },
+    {
+      name: 'serviço de informação ao cidadão',
+      link: '/servico-informacao-cidadao'
+    },
+    {
+      name: 'serviço online',
+      link: '/servico-online'
+    },
+    {
+      name: 'sessões',
+      link: '/sessoes'
     }
-     
+    
+
   ]
 }
