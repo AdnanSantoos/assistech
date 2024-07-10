7611a7db676db0bc86ad1a498cce4169e19fd559
Rayan Silva <rayan.lima@shopdelivery.com.br>
2024-07-10 10:58:50 -0300

    login

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,7 @@ import { CommonModule } from '@angular/common';
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private router: Router) {
     this.loginForm = this.fb.group({
       login: ['', Validators.required],
       senha: ['', Validators.required],
@@ -24,6 +25,13 @@ export class LoginComponent {
   onSubmit() {
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
+      const loginSuccessful = true;
+
+      if (loginSuccessful) {
+        this.router.navigate(['/dashboard']);
+      } else {
+        alert('Login ou senha inválidos');
+      }
     }
   }
 }
