b01b3496e7c9881e8e57fdf9385021e5eb55b67c
Rayan Silva <rayan.lima@shopdelivery.com.br>
2024-07-12 11:17:15 -0300

    menu administrativo

--- src/app/features/menu-administrativo/menu-administrativo.component.ts
+++ src/app/features/menu-administrativo/menu-administrativo.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu-administrativo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './menu-administrativo.component.html',
   styleUrl: './menu-administrativo.component.scss'
 })
 export class MenuAdministrativoComponent {
-
+  button = [
+    {
+      name: 'Diário Oficial',
+      link: '/'
+    },
+    {
+      name: 'PNCP',
+      link: '/'
+    },]
 }
