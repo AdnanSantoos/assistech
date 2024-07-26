1073e211e7a0af1918ab8fae9be8459a2509a281
Rayan Silva <rayan.lima@shopdelivery.com.br>
2024-07-26 14:48:53 -0300

    corrigindo slider com as imagens

--- src/app/shared/components/slider/slider.component.ts
+++ src/app/shared/components/slider/slider.component.ts
@@ -1,36 +1,64 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+interface Slide {
+  image: string;
+  title: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-slider',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.scss'
+  styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent {
-  slides = [
+  allSlides: Slide[] = [
     {
-      image: 'https://i.postimg.cc/wjDGwZf1/1400500.png',
+      image: '../../../assets/temporarias/Dia-do-Dentista.jpg',
       title: 'Dia do Médico Dermatologista',
       description: 'Sem pretender diminuir a importância de um generalista ou clínico geral bom, no entanto, é necessário destacar a imprescindibilidade dos especialistas. E hoje é dia do Médico Dermatologista. Derme: Pele – Logia: Estudo.'
     },
     {
-      image: 'https://i.postimg.cc/wjDGwZf1/1400500.png',
+      image: '../../../assets/temporarias/dia-do-medico.jpg',
       title: 'Dia do Médico Dermatologista',
       description: 'Sem pretender diminuir a importância de um generalista ou clínico geral bom, no entanto, é necessário destacar a imprescindibilidade dos especialistas. E hoje é dia do Médico Dermatologista. Derme: Pele – Logia: Estudo.'
     },
     {
-      image: 'https://i.postimg.cc/wjDGwZf1/1400500.png',
+      image: '../../../assets/temporarias/mpba.jpeg',
       title: 'Dia do Médico Dermatologista',
       description: 'Sem pretender diminuir a importância de um generalista ou clínico geral bom, no entanto, é necessário destacar a imprescindibilidade dos especialistas. E hoje é dia do Médico Dermatologista. Derme: Pele – Logia: Estudo.'
     },
     {
-      image: 'https://i.postimg.cc/wjDGwZf1/1400500.png',
+      image: '../../../assets/temporarias/publicitario-1.png',
+      title: 'Dia do Médico Dermatologista',
+      description: 'Sem pretender diminuir a importância de um generalista ou clínico geral bom, no entanto, é necessário destacar a imprescindibilidade dos especialistas. E hoje é dia do Médico Dermatologista. Derme: Pele – Logia: Estudo.'
+    },
+    {
+      image: '../../../assets/temporarias/vitiligo.png',
+      title: 'Dia do Médico Dermatologista',
+      description: 'Sem pretender diminuir a importância de um generalista ou clínico geral bom, no entanto, é necessário destacar a imprescindibilidade dos especialistas. E hoje é dia do Médico Dermatologista. Derme: Pele – Logia: Estudo.'
+    },
+    {
+      image: '../../../assets/temporarias/Servidor-Publico-dia.png',
       title: 'Dia do Médico Dermatologista',
       description: 'Sem pretender diminuir a importância de um generalista ou clínico geral bom, no entanto, é necessário destacar a imprescindibilidade dos especialistas. E hoje é dia do Médico Dermatologista. Derme: Pele – Logia: Estudo.'
     }
   ];
+
+  slides: Slide[] = [];
   currentSlide = 0;
+
+  constructor() {
+    this.slides = this.getRandomSlides(this.allSlides, 5);
+  }
+
+  getRandomSlides(slidesArray: Slide[], numSlides: number): Slide[] {
+    let shuffled = slidesArray.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, numSlides);
+  }
 
   setSlide(index: number) {
     this.currentSlide = index;
