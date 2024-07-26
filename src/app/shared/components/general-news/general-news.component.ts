25a5c8d5376d525faa13fd29b4b62e7aafcb2d30
Rayan Silva <rayan.lima@shopdelivery.com.br>
2024-07-26 14:54:41 -0300

    imagens das noticias gerais

--- src/app/shared/components/general-news/general-news.component.ts
+++ src/app/shared/components/general-news/general-news.component.ts
@@ -11,21 +11,21 @@ import { Component } from '@angular/core';
 export class GeneralNewsComponent {
   cards = [
     {
-      img: 'https://i.postimg.cc/wjDGwZf1/1400500.png',
+      img: '../../../assets/temporarias/noticia-01.png',
       data: 'xx/xx/xxx',
       title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
       subtitle: 'Lorem ipsum dolor',
       text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium, omnis similique? Eius placeat molestias explicabo voluptatibus sequi ad, voluptate veritatis voluptatum delectus voluptas praesentium nesciunt perferendis consequatur esse distinctio eveniet?'
     },
     {
-      img: 'https://i.postimg.cc/wjDGwZf1/1400500.png',
+      img: '../../../assets/temporarias/noticia-02.png',
       data: 'xx/xx/xxx',
       title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
       subtitle: 'Lorem ipsum dolor',
       text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium, omnis similique? Eius placeat molestias explicabo voluptatibus sequi ad, voluptate veritatis voluptatum delectus voluptas praesentium nesciunt perferendis consequatur esse distinctio eveniet?'
     },
     {
-      img: 'https://i.postimg.cc/wjDGwZf1/1400500.png',
+      img: '../../../assets/temporarias/noticia-03.png',
       data: 'xx/xx/xxx',
       title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
       subtitle: 'Lorem ipsum dolor',
