968a5eb439de74db0ab737e7a03105cf35fc9182
MESTRE\adnanpinho <adnanpinho@inovatio.com.br>
2024-08-14 23:15:22 -0300

    configs

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    apiUrl: 'https://api-staging.assistechpublicacoes.com.br/v1/',
+    tenant: 'admin'
+};
+
+// usuário: admin
+// email: admin@admin.com
+// senha: teste1
