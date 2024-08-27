9a2adc3dfbc65abe7e52e0f4b1f607519c9ba902
MESTRE\adnanpinho <adnanpinho@inovatio.com.br>
2024-08-27 12:39:11 -0300

    erro

--- src/app/features/login/services/login.service.ts
+++ src/app/features/login/services/login.service.ts
@@ -2,17 +2,23 @@ import { Router } from '@angular/router';
 import { LoginRepository } from '../repository/login.repository';
 import { LoginModel } from './../models/login.model';
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(private _repository:LoginRepository,private _router: Router) {}
+  constructor(private _repository:LoginRepository,private _router: Router, private toastr: ToastrService) {}
 
   public login(form:LoginModel){
-    this._repository.login(form).subscribe(()=>{
-      this._router.navigate(['/menu-administrativo']);
-    })
+    this._repository.login(form).subscribe({
+      next: (data) => {
+        this._router.navigate(['/menu-administrativo']);
+      },
+      error: (err) =>{
+        this.toastr.error(err.error.message,'Ocorreu um erro!');
+      }
+    });
   }
 }
