dd43fa2b2135a9a122697b5dd4167cfef92591e9
Rayan Silva Lima <rayan.lima@shopdelivery.com.br>
2024-09-26 14:28:39 -0300

    side bar 70%

--- src/app/shared/components/sidebar-administrativo/sidebar-administrativo.component.ts
+++ src/app/shared/components/sidebar-administrativo/sidebar-administrativo.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink, ActivatedRoute } from '@angular/router';
-import { MatExpansionModule } from '@angular/material/expansion';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar-administrativo',
   standalone: true,
-  imports: [CommonModule, RouterLink, MatExpansionModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './sidebar-administrativo.component.html',
   styleUrls: ['./sidebar-administrativo.component.scss'],
 })
 export class SidebarAdministrativoComponent {
+  // Objeto para manter o estado dos menus
+  expandedMenus: { [key: string]: boolean } = {};
+
+  // Função para alternar a visibilidade do submenu
+  toggleSubMenu(menuId: string) {
+    this.expandedMenus[menuId] = !this.expandedMenus[menuId];
+  }
+
+  // Verifica se o submenu está aberto
+  isSubMenuOpen(menuId: string): boolean {
+    return !!this.expandedMenus[menuId];
+  }
 }
