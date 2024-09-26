3912dce74f649df885ecb3a0318947498327419f
Rayan Silva Lima <rayan.lima@shopdelivery.com.br>
2024-09-26 14:28:53 -0300

    side bar 70%

--- src/app/shared/components/sidebar-administrativo/sidebar-administrativo.component.ts
+++ src/app/shared/components/sidebar-administrativo/sidebar-administrativo.component.ts
@@ -10,15 +10,12 @@ import { RouterLink } from '@angular/router';
   styleUrls: ['./sidebar-administrativo.component.scss'],
 })
 export class SidebarAdministrativoComponent {
-  // Objeto para manter o estado dos menus
   expandedMenus: { [key: string]: boolean } = {};
 
-  // Função para alternar a visibilidade do submenu
   toggleSubMenu(menuId: string) {
     this.expandedMenus[menuId] = !this.expandedMenus[menuId];
   }
 
-  // Verifica se o submenu está aberto
   isSubMenuOpen(menuId: string): boolean {
     return !!this.expandedMenus[menuId];
   }
