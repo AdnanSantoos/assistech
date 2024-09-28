82e5c9eae58f9c6417cae76213a76ef383c1ddd0
Rayan Silva <rayansl0302@gmail.com>
2024-09-28 13:03:17 -0300

    footer, header, sidebar e dashboard

--- src/app/features/dashboard-administrativo/dashboard-administrativo.component.ts
+++ src/app/features/dashboard-administrativo/dashboard-administrativo.component.ts
@@ -1,24 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SidebarAdministrativoComponent } from '../../shared/components/sidebar-administrativo/sidebar-administrativo.component';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-dashboard-administrativo',
   standalone: true,
-  imports: [CommonModule,SidebarAdministrativoComponent],
+  imports: [CommonModule,SidebarAdministrativoComponent,NavbarComponent],
   templateUrl: './dashboard-administrativo.component.html',
   styleUrl: './dashboard-administrativo.component.scss'
 })
 export class DashboardAdministrativoComponent {
   categorias = [
-    { nome: 'Publicações', quantidade: 4230 },
-    { nome: 'Usuários', quantidade: 1230 },
     { nome: 'Documentos', quantidade: 450 },
+    { nome: 'Usuários', quantidade: 1230 },
+    { nome: 'Publicações', quantidade: 4230 },
     { nome: 'Órgãos', quantidade: 20 },
     { nome: 'Contratos', quantidade: 75 },
-    { nome: 'Planos de Contratação', quantidade: 15 },
     { nome: 'Licitações', quantidade: 5 },
     { nome: 'Unidades', quantidade: 12 },
+    { nome: 'Planos de Contratação', quantidade: 15 },
+    { nome: 'Termos', quantidade: 8 },
 
   ];
 
