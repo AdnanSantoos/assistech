127ff2a913da11cf62b22eefd911f4ee4c835add
Rayan Silva <rayansl0302@gmail.com>
2024-09-29 17:19:16 -0300

    forms

--- src/app/shared/components/forms/forms.component.ts
+++ src/app/shared/components/forms/forms.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
@@ -50,11 +50,15 @@ export class FormsComponent implements OnInit {
   @Input() dynamicFields: any[] = [];
   @Input() form!: FormGroup;
   @Output() formSubmit = new EventEmitter<void>();
-  nameFile:string|null = null;
+  nameFile: string | null = null;
+  isEditing: boolean = false;
+
+  selectedFiles: File[] = [];
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
 
   constructor(private fb: FormBuilder, private dateAdapter: DateAdapter<Date>) {
     this.form = this.fb.group({});
-    this.dateAdapter.setLocale('pt-BR'); // Configura a localidade para pt-BR
+    this.dateAdapter.setLocale('pt-BR');
   }
 
   ngOnInit() {
@@ -71,12 +75,27 @@ export class FormsComponent implements OnInit {
 
   onFileChange(event: any, fieldName: string) {
     const file = event.target.files[0];
-    this.nameFile = file.name
-    this.form.patchValue({
-      [fieldName]: file,
-    });
-    this.form.get(fieldName)?.updateValueAndValidity();
+    if (file && file.type === 'application/pdf') {
+      this.selectedFiles = [file];
+      this.nameFile = file.name;
+      this.form.patchValue({
+        [fieldName]: this.selectedFiles,
+      });
+      this.form.get(fieldName)?.updateValueAndValidity();
+    } else {
+      alert('Apenas arquivos PDF são permitidos.');
+    }
+  }
 
+  editFile() {
+    this.fileInput.nativeElement.click();
+  }
+
+  removeFile(index: number) {
+    this.selectedFiles.splice(index, 1);
+    if (this.selectedFiles.length === 0) {
+      this.nameFile = null;
+    }
   }
 
   onSubmit() {
@@ -89,6 +108,8 @@ export class FormsComponent implements OnInit {
           formData[key] = this.form.get(key)?.value.toISOString();
         }
       });
+
+      console.log('Dados do formulário enviados:', formData);
       this.formSubmit.emit(formData);
     }
   }
