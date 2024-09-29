80680a2ddbd2e121736e9cd1c99fe8261526c917
Rayan Silva <rayansl0302@gmail.com>
2024-09-29 16:48:51 -0300

    atualizações layoutforms

--- src/app/shared/containers/layout-forms-adm/layout-forms-adm.component.ts
+++ src/app/shared/containers/layout-forms-adm/layout-forms-adm.component.ts
@@ -5,22 +5,31 @@ import { RouterLink } from '@angular/router';
 import { FormsComponent } from '../../components/forms/forms.component';
 import { SidebarAdministrativoComponent } from '../../components/sidebar-administrativo/sidebar-administrativo.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-layout-forms-adm',
   standalone: true,
-  imports: [CommonModule, RouterLink, FormsComponent,SidebarAdministrativoComponent,NavbarComponent],
+  imports: [CommonModule, RouterLink, FormsComponent, SidebarAdministrativoComponent, NavbarComponent, MatIcon],
   templateUrl: './layout-forms-adm.component.html',
   styleUrls: ['./layout-forms-adm.component.scss'],
 })
 export class LayoutFormsAdmComponent implements OnInit {
   @Input() dynamicFields: any[] = [];
   @Input() titulo_pagina: string | null = null;
+  @Input() titulo_form: string | null = null;
   @Input() form!: FormGroup;
   @Output() formSubmit = new EventEmitter<FormGroup>();
 
-  constructor(private fb: FormBuilder, private location: Location) {}
+  constructor(private fb: FormBuilder, private location: Location) { }
+
   ngOnInit() {
+    // Se o titulo_form estiver vazio ou nulo, atribuímos "Cadastrar" + titulo_pagina
+    if (!this.titulo_form || this.titulo_form.trim() === '') {
+      this.titulo_form = `Cadastrar ${this.titulo_pagina}`;
+    }
+
+    // Adicionando controles dinâmicos ao formulário
     this.dynamicFields.forEach((field) => {
       if (!this.form.contains(field.name)) {
         if (field.type === 'file') {
@@ -43,9 +52,11 @@ export class LayoutFormsAdmComponent implements OnInit {
       console.error('Nenhum arquivo foi selecionado.');
     }
   }
+
   goBack(): void {
     this.location.back();
   }
+
   onSubmit(form: any) {
     this.formSubmit.emit(form);
   }
