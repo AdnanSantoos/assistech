a55de60a29c6dadc51e91eb7c8d3533721ad073e
rayansl0302 <rayansl0302@users.noreply.github.com>
2024-12-05 11:59:16 +0000

    deploy: b437d7fa6405b519ea17e0b7931bd931b6371772

---
+++ browser/chunk-NCG5LLV2.js
@@ -0,0 +1,1 @@
+var t={apiUrl:"https://api-staging.assistechpublicacoes.com.br/v1",tenant:"admin"};export{t as a};
---
+++ server/chunk-AZ43DKBQ.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+var t={apiUrl:"https://api-staging.assistechpublicacoes.com.br/v1",tenant:"admin"};export{t as a};
