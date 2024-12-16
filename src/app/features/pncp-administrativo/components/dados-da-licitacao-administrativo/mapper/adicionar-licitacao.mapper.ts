e88764e260428e91e2e9bb5b7d61bea0db7e6c62
Rayan Silva <rayansl0302@gmail.com>
2024-12-15 21:55:57 -0300

    mapper adicionar licitacao

--- src/app/features/pncp-administrativo/components/dados-da-licitacao-administrativo/mapper/adicionar-licitacao.mapper.ts
+++ src/app/features/pncp-administrativo/components/dados-da-licitacao-administrativo/mapper/adicionar-licitacao.mapper.ts
@@ -3,10 +3,22 @@ export class AdicionarLicitacaoMapper {
     let value = {
       ...form
     }
+    const formData = new FormData();
+
     value.agency = {
       country_register: value.agency.unit.agency.country_register,
       name: value.agency.unit.agency.name
+    }
+    Object.keys(form).forEach((key) => {
+      formData.append(key, form[key]);
+    });
 
+    if (value.files && value.files.length > 0) {
+      value.files.forEach((file: File) => {
+        formData.append('files[]', file);
+      });
+    } else {
+      console.error("Nenhum arquivo foi selecionado.");
     }
     return value
   }
