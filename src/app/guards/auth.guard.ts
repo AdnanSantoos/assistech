a2d1223cc45026d219714fa93402cd214ad5f6ee
MESTRE\adnanpinho <adnanpinho@inovatio.com.br>
2025-01-28 23:24:15 -0300

    correção

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,19 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { TenantService } from '../shared/services/tenant.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+export class AuthGuard implements CanActivate, OnDestroy {
+  slug: string | null = null;
+  private destroy$ = new Subject<void>();
+  constructor(private router: Router,private tenantService: TenantService) {
+      this.tenantService.slug$.pipe(takeUntil(this.destroy$)).subscribe((slug) => {
+        if(this.slug){
+          return
+        }
+          this.slug = slug;
+      });
+  }
 
   canActivate(): boolean {
     const token = localStorage.getItem('authToken'); 
     if (token) {
       return true;
     } else {
-      this.router.navigate(['/adm/login']);
+      this.router.navigate(['/app', this.slug, 'adm', 'login']);
       return false;
     }
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
